b8090fe968758f38e75c461dd819c5463fe2fac9
zhangqm <qimeng_zhang@intsig.net>
2023-04-08 23:45:23 +0800

    feat:方法功能单一化

--- test/generate_dir_test.go
+++ test/generate_dir_test.go
@@ -18,31 +18,36 @@ type DirDesc struct {
 var currentDir string        //当前目录
 var workspaceRootDir string  //项目根目录
 var filePathSeparator string //路径分割符
-var jsonContent DirDesc      //json文件内容
 
 const dirJsonFileName = "dir.json"
 
-// 读取路径描述
-func loadJson() {
+// 初始化部分数据
+func init() {
 	currentDir, _ = os.Getwd()
 	filePathSeparator = string(filepath.Separator)
 	workspaceRootDir = currentDir[0:strings.LastIndex(currentDir, filePathSeparator)]
+}
+
+// 读取路径描述
+func loadJson() *DirDesc {
 	jsonFile, _ := os.ReadFile(strings.Join([]string{currentDir, dirJsonFileName}, filePathSeparator))
+	var jsonContent DirDesc
 	err := json.Unmarshal(jsonFile, &jsonContent)
 	if err != nil {
 		panic("序列化json文件失败：" + err.Error())
 	} else {
 		fmt.Println("读取文件成功")
 	}
+	return &jsonContent
 }
 
 // 根据树状数据返回需要创建对目录路径集合
-func parseMap(data *[]DirDesc, prePath string, result []string) []string {
+func parseDirDesc(data *[]DirDesc, prePath string, result []string) []string {
 	for _, val := range *data {
 		currentPath := strings.Join([]string{prePath, val.Text}, filePathSeparator)
 		result = append(result, currentPath)
 		if val.Children != nil {
-			return parseMap(val.Children, currentPath, result)
+			return parseDirDesc(val.Children, currentPath, result)
 		}
 	}
 	return result
@@ -58,8 +63,8 @@ func createDirByPaths(paths []string, baseDir string) {
 	}
 }
 func TestGenerateDir(t *testing.T) {
-	loadJson()
+	jsonContent := loadJson()
 	var paths []string
-	paths = parseMap(&[]DirDesc{jsonContent}, "", paths)
+	paths = parseDirDesc(&[]DirDesc{*jsonContent}, "", paths)
 	createDirByPaths(paths, workspaceRootDir)
 }
