5fbe396153d17d7fdd0a9d5707c0d84f32be7443
zhangqm <qimeng_zhang@intsig.net>
2023-09-03 00:22:52 +0800

    refactor: 移除获取IP的方法，改用Gin提供的方法

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,11 +1,7 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
-	"net"
-	"net/http"
-	"strings"
 )
 
 // 拼接错误
@@ -14,26 +10,4 @@ func AppendError(prev, next error) error {
 		return next
 	}
 	return fmt.Errorf("%v,%w", prev, next)
-}
-
-// 获取请求的ip
-func GetIP(r *http.Request) (string, error) {
-	ip := r.Header.Get("X-Real-IP")
-	if net.ParseIP(ip) != nil {
-		return ip, nil
-	}
-	ip = r.Header.Get("X-Forward-For")
-	for _, i := range strings.Split(ip, ",") {
-		if net.ParseIP(i) != nil {
-			return i, nil
-		}
-	}
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return "", err
-	}
-	if net.ParseIP(ip) != nil {
-		return ip, nil
-	}
-	return "", errors.New("未发现IP信息")
 }
