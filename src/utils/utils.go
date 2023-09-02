f6804ff7c1bc41b12540ab062d7f10fc2202742f
zhangqm <qimeng_zhang@intsig.net>
2023-09-02 23:53:29 +0800

    feat: 添加获取ip方法

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,10 +1,39 @@
 package utils
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+)
 
+// 拼接错误
 func AppendError(prev, next error) error {
 	if prev == nil {
 		return next
 	}
 	return fmt.Errorf("%v,%w", prev, next)
+}
+
+// 获取请求的ip
+func GetIP(r *http.Request) (string, error) {
+	ip := r.Header.Get("X-Real-IP")
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+	ip = r.Header.Get("X-Forward-For")
+	for _, i := range strings.Split(ip, ",") {
+		if net.ParseIP(i) != nil {
+			return i, nil
+		}
+	}
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+	return "", errors.New("未发现IP信息")
 }
