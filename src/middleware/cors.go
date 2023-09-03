3c48b08b0bdac2042513f656e201d736cf6765fc
zhangqm <qimeng_zhang@intsig.net>
2023-09-03 15:26:11 +0800

    feat: 开发环境允许cors

--- src/middleware/cors.go
+++ src/middleware/cors.go
@@ -3,11 +3,15 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOriginFunc: func(origin string) bool {
+			if viper.GetBool("mode.dev") {
+				return true
+			}
 			//TODO：站点限制
 			return true
 		},
